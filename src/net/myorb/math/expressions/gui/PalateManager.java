4f2ffaef8b5c1406500ec956ceed7d2a3e8c6a71
Michael Druckman <spydrcom@gmail.com>
2022-08-31 13:43:32 -0700

    Update PalateManager.java
    empty implementation, infrastructure put in place

--- src/net/myorb/math/expressions/gui/PalateManager.java
+++ src/net/myorb/math/expressions/gui/PalateManager.java
@@ -1,16 +1,50 @@
 
 package net.myorb.math.expressions.gui;
 
+import net.myorb.gui.components.SimpleScreenIO;
+import net.myorb.gui.components.DisplayFrame;
+
+import net.myorb.gui.palate.PalateTool;
+
+import javax.swing.JComponent;
+
 /**
  * manage the availability of the palate tool
  * @author Michael Druckman
  */
-public class PalateManager
+public class PalateManager extends PalateTool
 {
 
-	static void showPalateTool ()
+
+	/**
+	 * construct on first reference or force to screen
+	 */
+	public static void showPalateTool ()
 	{
-		throw new RuntimeException ("unimplemented");
-	} //TODO: unimplemented
+		if (palateToolDisplay == null)
+		{
+			showFrame (new PalateManager ());
+		}
+		else
+		{
+			palateToolDisplay.forceToScreen ();
+		}
+	}
+
+
+	/**
+	 * @param component the display component for the tool
+	 */
+	public static void showFrame (JComponent component)
+	{
+		palateToolDisplay =
+			new DisplayFrame (component, "Palate Tool");
+		component.setPreferredSize (SimpleScreenIO.wXh (900, 500));
+		palateToolDisplay.showOrHide ();
+	}
+	static DisplayFrame palateToolDisplay = null;
+
+
+	private static final long serialVersionUID = -822309604759704569L;
 
 }
