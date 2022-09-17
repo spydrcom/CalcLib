4cd0880e6c164082a4857539a39e59c27d99a922
Michael Druckman <spydrcom@gmail.com>
2022-09-17 13:45:34 -0700

    Update Combinatorics.java
    added method for Binomial using LogGamma

--- src/net/myorb/math/computational/Combinatorics.java
+++ src/net/myorb/math/computational/Combinatorics.java
@@ -214,6 +214,37 @@ public class Combinatorics<T>  extends Tolerances<T>
 			);
 	}
 
+	/**
+	 * binomial coefficients
+	 * - general case for all data types
+	 * - lnGAMMA used to extend to larger values
+	 * @param x the upper number of the set
+	 * @param y the lower number of the set
+	 * @return LnGAMMA based computation
+	 */
+	public T lnGammaBinomialCoefficient (T x, T y)
+	{
+		T	one = manager.getOne (),
+			ny	= manager.negate (y),
+			x1	= manager.add (x, one),
+			xy	= manager.add (x1, ny),
+			y1	= manager.add (y, one);
+
+		return lib.exp (manager.add
+			(
+				lib.ln (lib.GAMMA (x1)),
+			//  ----------------
+				manager.negate
+				(
+					manager.add
+					(
+						lib.ln (lib.GAMMA (y1)),
+						lib.ln (lib.GAMMA (xy))
+					)
+				)
+			));
+	}
+
 
 	/*
 	 * Bernoulli
