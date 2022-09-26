216f892bdf96cf958e8db15dd7cfb6f5533f20b4
Michael Druckman <spydrcom@gmail.com>
2022-09-26 12:56:39 -0700

    Update IterationConsumerImporter.java
    comments added

--- src/net/myorb/math/expressions/symbols/IterationConsumerImporter.java
+++ src/net/myorb/math/expressions/symbols/IterationConsumerImporter.java
@@ -1,7 +1,14 @@
 
 package net.myorb.math.expressions.symbols;
 
+/**
+ * access to an iteration consumer object for a system
+ * @author Michael Druckman
+ */
 public interface IterationConsumerImporter
 {
+	/**
+	 * @return the implemented iteration consumer object
+	 */
 	IterationConsumer getIterationConsumer ();
 }
