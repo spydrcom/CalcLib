5cbdde81c1529317382c2c8d30ba6e50cbf995e1
Michael Druckman <spydrcom@gmail.com>
2022-09-27 14:37:44 -0700

    spline based on Chebyshev nodes
    unit test added for VCN22
    matrix files copied

---
+++ testing/net/myorb/testing/linalg/VCN22.java
@@ -0,0 +1,27 @@
+
+package net.myorb.testing.linalg;
+
+import net.myorb.math.computational.Parameterization;
+import net.myorb.math.realnumbers.RealFunctionWrapper;
+
+import net.myorb.math.computational.VCN22LUD;
+import net.myorb.math.Function;
+
+public class VCN22 extends VCN22LUD
+{
+
+	VCN22 ()
+	{
+		super (new Parameterization ());
+	}
+
+	static double f (double x) { return 2*Math.pow(x,3) - 3*Math.pow(x,2); }
+
+	public static void main (String... args)
+	{
+		Function <Double> f = new RealFunctionWrapper ( (x) -> f(x) ).toCommonFunction ();
+
+		System.out.println (new VCN22 ().spline (f, -1d, 1d));
+	}
+
+}
