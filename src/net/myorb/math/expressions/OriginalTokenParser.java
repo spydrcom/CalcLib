98a1eb2fcef0aaabf212a5f9b9e2e402105d6327
Michael Druckman <spydrcom@gmail.com>
2022-09-27 11:47:18 -0700

    Update OriginalTokenParser.java
    lambdas and blocks now working together

--- src/net/myorb/math/expressions/OriginalTokenParser.java
+++ src/net/myorb/math/expressions/OriginalTokenParser.java
@@ -26,10 +26,11 @@ public class OriginalTokenParser extends CommonCommandParser
 	
 	public String getIdnLead () { return IDN_LEAD; }
 	public String getWhiteSpace () { return WHITE_SPACE; }
-	public String getSequenceCaptureMarkers () { return null; }
 	public String getMultiCharacterOperator () { return MULTI_CHARACTER_OPERATOR; }
 	public String getExtendedOperator () { return OPERATOR_EXTENDED; }
 	public Collection<String> getCommentIndicators () { return null; }
+	public String getCaptureStartMarker () { return null; }
+	public String getCaptureEndMarker () { return null; }
 	public String getOperator () { return OPERATOR; }
 	public String getIdnBody () { return IDN_BODY; }
 	
