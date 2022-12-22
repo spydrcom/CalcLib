ff4851b5edd3f5ffe16f4102029c599ef587d4c3
Michael Druckman <spydrcom@gmail.com>
2022-12-22 15:13:43 -0800

    Update IterativeAlternativeAlgorithmTests.java
    test computing ln PHI from chi2 PHI added to demonstrate truncate method
    computation is performed on reduce(chiPhi) to avoid computation overload
    precision is shown to be maintained and execution time is not painful
    algorithm requires use of SQRT function which has been added to NR
    precision is being truncated at 25 decimal places
    so test show better than hardware precision

--- testing/net/myorb/testing/factors/IterativeAlternativeAlgorithmTests.java
+++ testing/net/myorb/testing/factors/IterativeAlternativeAlgorithmTests.java
@@ -27,8 +27,11 @@ public class IterativeAlternativeAlgorithmTests
 		IterativeAlternativeAlgorithmTests testScripts =
 			new IterativeAlternativeAlgorithmTests ();
 		//testScripts.enableTracing ();
+		
+		testScripts.enableTracing ();
 
 		testScripts.computeSqrt ();
+		testScripts.computePi ();
 
 		testScripts.runChiTests ();
 
@@ -47,15 +50,51 @@ public class IterativeAlternativeAlgorithmTests
 		// chi2 (1)  =  PI^2/8  =  1.2337005501361698273543113749845
 
 		computePhi ();
-		runChiTest (IT.sumOf (phi, IT.S (-1)), 80, AccuracyCheck.ChiPhi_REF, "CHI2(phi-1)");
+		Factorization chiPhi = 
+			runChiTest (IT.sumOf (reduce (phi), IT.S (-1)), 80, AccuracyCheck.ChiPhi_REF, "CHI2(phi-1)");
 		// chi2(PHI-1)  =  PI^2/12 - 3/4 [ln(PHI)]^2  =  0.64879341799121742386351077989936
+		runLnPhiTest (reduce (chiPhi));
 	}
-	void runChiTest (Factorization of, int iterations, String ref, String tag)
+	Factorization runChiTest (Factorization of, int iterations, String ref, String tag)
 	{
 		Factorization chi = run
-		(iterations, IT.getChi2DerivativeComputer (), of);
+			(iterations, IT.getChi2DerivativeComputer (), of);
 		display (chi, ref, tag);
 		timeStamp ();
+		return chi;
+	}
+
+
+	/**
+	 * computation of ln(PHI)
+	 */
+	void runLnPhiTest (Factorization chi)
+	{
+		display
+			(
+				chi, AccuracyCheck.ChiPhi_REF,
+				"REDUCED CHI ( as factor of ln[PHI] )"
+			);
+		Factorization lnPhi = computeLnPhiTest (chi);
+		display (lnPhi, AccuracyCheck.LnPhi_REF, "ln PHI");
+		timeStamp ();
+	}
+	Factorization computeLnPhiTest (Factorization chi)
+	{
+		Factorization
+			piSQ = IT.POW (getReducedPi (), 2),
+			piSQ12  = IT.productOf (IT.oneOver (IT.S (12)), piSQ),
+			lnPhi34 = IT.sumOf (piSQ12, manager.negate (chi));				// PI^2/12 - chi
+		Factorization
+			lnPhi3  = IT.productOf (reduce (lnPhi34), IT.S (4)),
+			lnPhiSQ = IT.productOf (lnPhi3, IT.oneOver (IT.S (3)));			// 4/3 * [ PI^2/12 - chi ]
+		return NR.sqrt (lnPhiSQ, ROOT_ITERATIONS);
+	}
+	Factorization reduce (Factorization x)					// truncation to 25 decimal places
+	{
+		return FactorizationCore.mgr
+			.getPrecisionManager ()
+			.truncate (x, 25);	
 	}
 
 
@@ -66,8 +105,8 @@ public class IterativeAlternativeAlgorithmTests
 	{
 		JsonLowLevel.JsonValue json = null;
 		try { json = readFile (); } catch (Exception e) {}
-		Factorization pi = FactorizationCore.mgr.fromJson (json);
-		System.out.println (pi);
+		Factorization pi = reduce (FactorizationCore.mgr.fromJson (json));
+		System.out.println ("Reduced PI = " + pi);
 		return pi;
 	}
 	JsonLowLevel.JsonValue readFile () throws Exception
