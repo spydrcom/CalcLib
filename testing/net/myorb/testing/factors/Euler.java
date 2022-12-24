1827d8387a491b157f5d0bdbd6a69ca1cd9556e1
Michael Druckman <spydrcom@gmail.com>
2022-12-23 16:09:34 -0800

    Create Euler.java
    unit tests for Bernoulli and Euler number series algorithms
    tests added for Taylor series of TAN and SEC using same
    Factorization and Double versions both tested
    BEWARE iteration counts when Double used
    round-off error also seen with Double

---
+++ testing/net/myorb/testing/factors/Euler.java
@@ -0,0 +1,107 @@
+
+package net.myorb.testing.factors;
+
+import net.myorb.math.computational.iterative.IterationTools;
+import net.myorb.math.expressions.managers.ExpressionFloatingFieldManager;
+import net.myorb.math.computational.iterative.Taylor;
+//import net.myorb.math.primenumbers.Factorization;
+
+/**
+ * tests for numeric series used in Taylor tests
+ * - tangent and secant Taylor series require Bernoulli and Euler series respectively
+ * @author Michael Druckman
+ */
+public class Euler extends IterativeCompoundAlgorithmTests
+{
+
+
+	/**
+	 * entry point for running the test
+	 * @param a not used
+	 */
+	public static void main (String[] a)
+	{
+
+		//numbersTest ();
+
+		//secantTest ();
+
+		tanTest ();
+
+	}
+
+
+	/**
+	 * testing Bernoulli numbers indirectly by running tangent calculations
+	 */
+	static void tanTest ()
+	{
+		ExpressionFloatingFieldManager manager = new ExpressionFloatingFieldManager ();
+		IterationTools <Double> IT = new IterationTools <Double> (); IT.setManager (manager);
+		Taylor <Double> taylor = new Taylor <Double> (manager); taylor.enableTracing ();
+		double tan = taylor.run (30, IT.getTanDerivativeComputer (), Math.PI/12);
+		// 0.26794919243112270647255365849413
+		System.out.println (tan);
+	}
+
+
+	/**
+	 * testing Euler numbers indirectly by running secant calculations
+	 */
+	static void secantTest ()
+	{
+		ExpressionFloatingFieldManager manager = new ExpressionFloatingFieldManager ();
+		IterationTools <Double> IT = new IterationTools <Double> (); IT.setManager (manager);
+		Taylor <Double> taylor = new Taylor <Double> (manager); taylor.enableTracing ();
+		double sec = taylor.run (30, IT.getSecDerivativeComputer (), Math.PI/12);
+		// 1.0352761804100830493955953504962
+		System.out.println (sec);
+	}
+
+
+	/**
+	 * run tests of Euler and Bernoulli series
+	 */
+	static void numbersTest ()
+	{
+		Euler testScripts = new Euler ();
+		testScripts.IT.setManager (FactorizationCore.mgr);
+		testScripts.runNumbers ();
+	}
+
+
+	/**
+	 * run computation of Euler and Bernoulli series
+	 */
+	void runNumbers ()
+	{
+		System.out.println ("Euler");
+		for (int n = 0; n < 20; n++)
+			System.out.println 
+			(
+				manager.toDecimalString (IT.combo.E2nDoubleSum (n))
+			);
+		System.out.println ("Bernoulli");
+		for (int n = 0; n < 20; n++)
+			System.out.println 
+			(
+				manager.toDecimalString (IT.combo.firstKindBernoulli (n))
+			);
+	}
+
+	/*
+	 * E2n
+	 *  0   2  4    6     8      10       12
+		1, -1, 5, -61, 1385, -50521, 2702765, -199360981, 19391512145, -2404879675441, 
+		370371188237525, -69348874393137901, 15514534163557086905, -4087072509293123892361, 
+		1252259641403629865468285, -441543893249023104553682821, 
+		177519391579539289436664789665
+	 */
+
+	/*
+	 * E2n
+	 *  0     1    2  2      4  5     6  7      8  9    10
+	    1   1/2  1/6  0  -1/30  0  1/42  0  -1/30  0  5/66
+	 */
+
+}
