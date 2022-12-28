4e484283df75006c83101042bee6aa3ff427f701
Michael Druckman <spydrcom@gmail.com>
2022-12-28 15:08:29 -0800

    Update Euler.java
    test artifact

--- testing/net/myorb/testing/factors/Euler.java
+++ testing/net/myorb/testing/factors/Euler.java
@@ -85,9 +85,9 @@ public class Euler extends IterativeCompoundAlgorithmTests
 		for (int n = 0; n < 20; n+=2)
 			System.out.println 
 			(
-//					"E2nD  "+manager.toDecimalString (IT.combo.E2nDoubleSum (n))
+					"E2nD  "+manager.toDecimalString (IT.combo.E2nDoubleSum (n))
 //					"E2n   "+manager.toDecimalString (IT.combo.E2n (n))
-					"En    "+manager.toDecimalString (IT.combo.En (n))
+//					"En    "+manager.toDecimalString (IT.combo.En (n))
 			);
 	}
 	/*
