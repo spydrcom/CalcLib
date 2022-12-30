2e305468ecbf37c22f14c8ed80ad6415c9f0bcde
Michael Druckman <spydrcom@gmail.com>
2022-12-30 14:45:18 -0800

    Update NewtonRaphsonIterativeTest.java
    full study of effect of precision monitor on execution efficiency
    some refactor of iteration processing

--- testing/net/myorb/testing/factors/NewtonRaphsonIterativeTest.java
+++ testing/net/myorb/testing/factors/NewtonRaphsonIterativeTest.java
@@ -43,6 +43,45 @@ public class NewtonRaphsonIterativeTest extends NewtonRaphson <Factorization>
 	protected int [] functionDescription;
 
 
+	/*
+
+			Requested	Iterations		Elapsed		Resulting
+			Precision	(short-circuit)	Time		Precision
+			=========   =============== =======     =========
+
+			 10			 5				 340ms			  12
+			 20			 7				 370ms			  22
+			 30			 7				 550ms			  33
+			 40			 7				 880ms			  42
+			 50			 8				 900ms			  54
+			 60			 8				 930ms			  62
+			 70			 8				1100ms			  73
+			 80			 8				1130ms			  83
+			 90			 9				1260ms			  93
+			100			10				1500ms			 102
+			120			 9				1700ms			 124
+			150			 9				1900ms			 154
+			200			11				3100ms			 206
+			300			10				4650ms			 303
+
+			 0			11				 950ms			1569
+			 0			12				2460ms			3136
+
+			times are averages done of several runs...
+			cross-over appears to be 60 digits of requested precision.
+
+			the precision manipulations significantly reduce time efficiency.
+			for numbers >60 it seem better strategy to just take results set by iteration count...
+			and then apply a precision reduction of the longer result for subsequent uses.
+
+	 */
+	static final int
+		MAX_PRECISION		= 300,		//	00 gives unrestricted
+		MAX_DIGIT_COMPARE	= 1000,
+		MAX_ITERATIONS		= 12
+	;
+
+
 	/**
 	 * entry point for running the test
 	 * - simple version of stand-alone test sqrt(2)
@@ -54,25 +93,39 @@ public class NewtonRaphsonIterativeTest extends NewtonRaphson <Factorization>
 		Factorization approx;
 
 		FactorizationCore.init (1_000_000);
+		approx = new NewtonRaphsonIterativeTest (2)		// testing SQRT 2
+		.establishParameters (MAX_PRECISION)
+		.run (MAX_ITERATIONS);
 
-		approx = new NewtonRaphsonIterativeTest (2).establishParameters (MAX_PRECISION).run (11);
-
-		FactorizationCore.display (approx, AccuracyCheck.S2_REF, "SQRT", 2000);
+		FactorizationCore.display
+			(
+				approx,
+				AccuracyCheck.S2_REF, "SQRT",			// SQRT 2 reference
+				MAX_DIGIT_COMPARE
+			);
+		FactorizationCore.timeStamp ();
 
 	}
-	static final int MAX_PRECISION = 30;
 
+	/**
+	 * install Precision Monitor
+	 * @param precision requested digits as metric of accuracy
+	 * @return THIS object
+	 */
 	NewtonRaphsonIterativeTest establishParameters (int precision)
 	{ return establishParameters (FactorizationCore.mgr, precision); }
 
 	NewtonRaphsonIterativeTest establishParameters
 		(ExpressionFactorizedFieldManager m, int precision)
 	{
-		this.installPrecisionMonitor
-		(
-			m.getPrecisionManager (),
-			m, precision
-		);
+		if (precision > 0)
+		{
+			this.installPrecisionMonitor
+			(
+				m.getPrecisionManager (),
+				m, precision
+			);
+		}
 		return this;
 	}
 
@@ -96,20 +149,20 @@ public class NewtonRaphsonIterativeTest extends NewtonRaphson <Factorization>
 	 * @param iterations number of iterations to run
 	 * @return the computed result after specified iterations
 	 */
-	public Factorization run (int iterations)
+	public Factorization run ( int iterations )
+	{ initializeFunction (); runForCount (iterations); return getX (); }
+	public Factorization run ( int iterations,  GeneratingFunctions.Coefficients <Factorization>  C )
+	{ initializeFunction (C); runForCount (iterations); return getX (); }
+
+	/**
+	 * trap short-circuit condition
+	 * - allowing controlled termination of loop
+	 * @param iterations number of iterations to run
+	 */
+	void runForCount (int iterations)
 	{
-		initializeFunction ();
-		try { for (int i = 1; i <= iterations; i++) { iterate (i); } }
-		catch (ShortCircuitTermination e) { System.out.println (e.getMessage ()); }
-		return getX ();
-	}
-	public Factorization run
-	(int iterations, GeneratingFunctions.Coefficients<Factorization> C)
-	{
-		initializeFunction (C);
-		try { for (int i = 1; i <= iterations; i++) { iterate (i); } }
-		catch (ShortCircuitTermination e) { System.out.println (e.getMessage ()); }
-		return getX ();
+		try { for ( int i = 1; i <= iterations; i++ ) { iterate (i); } }
+		catch (ShortCircuitTermination e) { if (tracing) System.out.println (e.getMessage ()); }
 	}
 
 
@@ -121,7 +174,10 @@ public class NewtonRaphsonIterativeTest extends NewtonRaphson <Factorization>
 		applyIteration ();
 		this.testForShortCircuit
 		(this.getDelta (), iteration, manager);
-		if ( ! tracing ) return;
+		if (tracing) traceIteration ();
+	}
+	void traceIteration ()
+	{
 		testVal = IT.POW (getX (), 2);
 		System.out.println (this);
 		System.out.println ();
