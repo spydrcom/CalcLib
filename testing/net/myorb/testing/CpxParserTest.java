5ceb3106dc1497fd78e528d1264f6635d0b1d9bc
Michael Druckman <spydrcom@gmail.com>
2023-01-25 14:51:06 -0800

    Create CpxParserTest.java
    simple test for complex parser

---
+++ testing/net/myorb/testing/CpxParserTest.java
@@ -0,0 +1,12 @@
+package net.myorb.testing;
+
+import net.myorb.math.complexnumbers.*;
+
+public class CpxParserTest {
+
+	public static void main (String... args)
+	{
+		System.out.println (ComplexSpaceCore.parseComplex("3.5-!*2.2"));
+	}
+
+}
