bf44293bbbbbed0e9919515ed73d8a1a9bad1812
Michael Druckman <spydrcom@gmail.com>
2023-01-27 12:58:06 -0800

    Update Lerch.java
    Transcendent interface added to focus definition of PHI
    Series sub-class added as standard implementation of PHI
    comments and minor code changes to series implementation

--- src/net/myorb/math/specialfunctions/Lerch.java
+++ src/net/myorb/math/specialfunctions/Lerch.java
@@ -13,10 +13,59 @@ public class Lerch extends ComplexSpaceCore
 
 
 	/**
+	 * definition of the Lerch Transcendent (PHI) function
+	 */
+	public interface Transcendent
+	{
+		/**
+		 * Lerch transcendent (PHI) function
+		 * @param z parameter to the function call
+		 * @param s the order of the described function
+		 * @param alpha offset term for series denominator
+		 * @return the computed function result
+		 */
+		ComplexValue <Double> PHI
+		(
+			ComplexValue <Double> z,
+			ComplexValue <Double> s,
+			ComplexValue <Double> alpha
+		);
+	}
+
+
+	/**
+	 * implementation of Transcendent.PHI using series
+	 */
+	public static class Series implements Transcendent
+	{
+		public Series
+			(
+				ComplexValue <Double> tolerance,
+				ComplexValue <Double> termMaximum
+			)
+		{
+			this.tolerance = tolerance; this.terms = termMaximum;
+		}
+		public ComplexValue <Double> PHI
+			(
+				ComplexValue <Double> z,
+				ComplexValue <Double> s,
+				ComplexValue <Double> alpha
+			)
+		{
+			return Lerch.PHI (z, s, alpha, tolerance, terms);
+		}
+		protected ComplexValue <Double> tolerance, terms;
+	}
+
+
+	/**
 	 * Lerch transcendent (PHI) function
-	 * @param z parameter to the function
-	 * @param s the order of the function
+	 * - limited form of the infinite series
+	 * @param z parameter to the function call
+	 * @param s the order of the described function
 	 * @param alpha offset term for series denominator
+	 * @param tolerance the shortcut point of the terms
 	 * @param terms number of terms to apply to the series
 	 * @return the computed function result
 	 */
@@ -25,28 +74,43 @@ public class Lerch extends ComplexSpaceCore
 			ComplexValue <Double> z,
 			ComplexValue <Double> s,
 			ComplexValue <Double> alpha,
+			ComplexValue <Double> tolerance,
 			ComplexValue <Double> terms
 		)
 	{
-		ComplexValue <Double> sum = manager.getZero ();
+		boolean ignoreTolerance = tolerance.Re () <= 0;
+		ComplexValue <Double> sum = manager.getZero (), term;
 
 		for (int n = 0; n <= terms.Re (); n++)
 		{
 			sum = sumOf
 				(
 					sum,
-					term (z, s, alpha, n)
+					term = nTHterm (z, s, alpha, n)
 				);
+			if ( ignoreTolerance ) continue;
+			if ( withinTolerance (term, tolerance) ) return sum;
 		}
 
 		return sum;
 	}
+	static boolean withinTolerance
+	(ComplexValue <Double> term, ComplexValue <Double> tolerance)
+	{ return manager.lessThan (term, tolerance); }
 
 	/*
 	 * PHI (z,s,a) = SIGMA [ 0 <= n <= INFINITY ] ( z^n / ( n + a ) ^ s )
 	 */
 
-	public static ComplexValue <Double> term
+	/**
+	 * computation of one term
+	 * @param z parameter to the function call
+	 * @param s the order of the described function
+	 * @param a offset term for series denominator
+	 * @param n the index of the term
+	 * @return the computed value
+	 */
+	public static ComplexValue <Double> nTHterm
 		(
 			ComplexValue <Double> z,
 			ComplexValue <Double> s,
@@ -65,6 +129,14 @@ public class Lerch extends ComplexSpaceCore
 	 * 		( t^(s-1) / [(1 - z*exp(-t)) * exp(a*t)] * <*> t )
 	 */
 
+	/**
+	 * compute a value of the PHI integrand
+	 * @param z parameter to the function call
+	 * @param s the order of the described function
+	 * @param a offset term for series denominator
+	 * @param t the integration variable
+	 * @return the computed value
+	 */
 	public static ComplexValue <Double> integrand
 		(
 			ComplexValue <Double> z,
