65d4d13fe8c881c176aa907133e38cf53a1b1732
Michael Druckman <spydrcom@gmail.com>
2023-03-02 16:03:58 -0800

    Update TreeClone.java
    J0 and J1 series expansions coded for test cases

--- testing/net/myorb/testing/TreeClone.java
+++ testing/net/myorb/testing/TreeClone.java
@@ -2,7 +2,7 @@
 package net.myorb.testing;
 
 import net.myorb.math.polynomial.algebra.*;
-
+import net.myorb.math.polynomial.algebra.Elements.Factor;
 import net.myorb.math.expressions.ExpressionSpaceManager;
 
 import net.myorb.math.expressions.evaluationstates.Environment;
@@ -20,7 +20,11 @@ public class TreeClone
 			mgr = new ExpressionFloatingFieldManager ();
 		Environment <Double> environment = new Environment <> (mgr);
 
-		Gardener <Double> G1, G2;
+		Gardener <Double> G0, G1, G2;
+		G0 = Gardener.loadFromJson ("FSE", environment);
+		JsonSemantics.JsonValue tree0 = G0.getExpression ().toJson ();
+		Elements.Equation eqn0 = RepresentationConversions.translate (tree0);
+
 		G1 = Gardener.loadFromJson ("FSE1", environment);
 		JsonSemantics.JsonValue tree1 = G1.getExpression ().toJson ();
 		Elements.Equation eqn1 = RepresentationConversions.translate (tree1);
@@ -34,18 +38,70 @@ public class TreeClone
 //		System.out.println (G.getExpressionName ());
 //		System.out.println (G.getExpression ());
 
+		System.out.println (eqn0);
 		System.out.println (eqn1);
 		System.out.println (eqn2);
 
-		System.out.println (Operations.productOf (new Elements.Constant ("654"), eqn2));
-		System.out.println (Operations.productOf (new Elements.Variable ("xyz"), eqn2));
-		System.out.println (Operations.productOf (eqn1, eqn2));
+//		System.out.println (Operations.productOf (new Elements.Constant ("654"), eqn2));
+//		show (Operations.productOf (new Elements.Constant ("654"), eqn2));
+//
+//		System.out.println (Operations.productOf (new Elements.Variable ("xyz"), eqn2));
+//		show (Operations.productOf (new Elements.Variable ("xyz"), eqn2));
+//
+//		System.out.println (Operations.productOf (eqn1, eqn2));
+//		show (Operations.productOf (eqn1, eqn2));
+//
+//		Elements.Sum eqn3 = new Elements.Sum ();
+//		Elements.add (new Elements.Variable ("x"), eqn3);
+//		Elements.add (new Elements.Constant ("1"), eqn3);
+//
+//		System.out.println (Operations.productOf (eqn3, eqn2));
+//		show (Operations.productOf (eqn3, eqn2));
 
-		Elements.Sum eqn3 = new Elements.Sum ();
-		Elements.add (new Elements.Constant ("x"), eqn3);
-		Elements.add (new Elements.Variable ("1"), eqn3);
-		System.out.println (Operations.productOf (eqn3, eqn2));
+//		!! FSEJN (x) = ( x^2 * FSE2(x) ) + ( x * FSE1(x) ) + ( (x^2-n^2) * FSE(x) )
 
+		Factor
+			x = Elements.powerFactor ("x", 1.0),
+			x2 = Elements.powerFactor ("x", 2.0);
+		Elements.Sum J0 = new Elements.Sum ();
+
+		Elements.add (Operations.productOf (x, eqn1), J0);
+		Elements.add (Operations.productOf (x2, eqn0), J0);
+		Elements.add (Operations.productOf (x2, eqn2), J0);
+
+		System.out.println ();
+		System.out.println ("===");
+		System.out.println ("= J0 =");
+		System.out.println ("===");
+		show (J0);
+
+		Elements.Sum x2n2 = new Elements.Sum ();
+		Elements.add (new Elements.Constant ("-1"), x2n2);
+		Elements.add (x2, x2n2);
+
+		Elements.Sum J1 = new Elements.Sum ();
+		Elements.add (Operations.productOf (x, eqn1), J1);
+		Elements.add (Operations.productOf (x2n2, eqn0), J1);
+		Elements.add (Operations.productOf (x2, eqn2), J1);
+
+		System.out.println ();
+		System.out.println ("===");
+		System.out.println ("= J1 =");
+		System.out.println ("===");
+		show (J1);
+
+	}
+
+	public static void show (Elements.Factor eqn)
+	{
+		System.out.println
+		(
+			Manipulations.reduceAndCollectTerms
+			(
+				(Elements.Sum) eqn, "x"
+			)
+		);
+		System.out.println ("===");
 	}
 
 }
