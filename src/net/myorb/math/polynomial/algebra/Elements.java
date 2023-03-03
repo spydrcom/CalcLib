49ae6c75f9ba59af5b531b19f03668289810085c
Michael Druckman <spydrcom@gmail.com>
2023-03-02 16:01:53 -0800

    Update Elements.java
    complexity management implementation added

--- src/net/myorb/math/polynomial/algebra/Elements.java
+++ src/net/myorb/math/polynomial/algebra/Elements.java
@@ -19,14 +19,17 @@ public class Elements
 
 	// atomic Factor description base
 
+	public static interface Reference { boolean refersTo (String symbol); }
 	public static interface Factor { OpTypes getType (); }
+
 
 	// binary operation factors
 
 	/**
 	 * child nodes of multi-factor operations
 	 */
-	public abstract static class Factors extends ArrayList <Factor> implements Factor
+	public abstract static class Factors
+			extends ArrayList <Factor> implements Factor
 	{ private static final long serialVersionUID = 35114701359602093L; }
 
 	/**
@@ -34,9 +37,17 @@ public class Elements
 	 */
 	public static class Sum extends Factors
 	{
-		public String toString () { return image (this, "+"); }
+		public String toString () { return bracketedImage (this, " + "); }
 		private static final long serialVersionUID = -5102897249367062053L;
 		public OpTypes getType () { return OpTypes.Summation; }
+	}
+
+	/**
+	 * sum extension indicating subtraction
+	 */
+	public static class Difference extends Sum
+	{
+		private static final long serialVersionUID = -5098433414832709926L;
 	}
 
 	/**
@@ -63,6 +74,8 @@ public class Elements
 				value.substring (0, value.length () - 2) :
 				value;
 		}
+		public double getValue ()
+		{ return Double.parseDouble (value); }
 		public OpTypes getType () { return OpTypes.Operand; }
 		public Constant (String value) { this.value = value; }
 		String value;
@@ -71,25 +84,40 @@ public class Elements
 	/**
 	 * a factor made of a symbolic identifier
 	 */
-	public static class Variable implements Factor
+	public static class Variable implements Factor, Reference
 	{
 		public String toString () { return identifier; }
 		public OpTypes getType () { return OpTypes.Operand; }
-		public Variable (String identifier) { this.identifier = identifier; }
+		public boolean refersTo (String symbol)
+		{ return identifier.equals (symbol); }
+		public Variable (String identifier)
+		{ this.identifier = identifier; }
 		String identifier;
 	}
 
 	/**
 	 * a description of base-to-exponent factors
 	 */
-	public static class Power extends Factors
+	public static class Power extends Factors implements Reference
 	{
 		public Power () {}
+		public boolean refersTo (String symbol)
+		{ return ( (Reference) base () ).refersTo (symbol); }
+
 		private static final long serialVersionUID = -7726638099953294189L;
 		public String toString () { return base () + "^" + exponent (); }
+
 		public OpTypes getType () { return OpTypes.Operand; }
 		public Factor exponent () { return this.get (1); }
 		public Factor base () { return this.get (0); }
+
+		public static Factor reference (Variable variable, Double order)
+		{
+			Factors power = new Power ();
+			Constant exp = new Constant (order.toString ());
+			power.add (variable); power.add (exp);
+			return power;
+		}
 	}
 
 
@@ -126,6 +154,93 @@ public class Elements
 	}
 
 
+	/**
+	 * construct a reference to a variable
+	 * @param variable the identifier referenced
+	 * @param order the polynomial term order
+	 * @return the appropriate reference to
+	 */
+	public static Factor powerFactor (String variable, Double order)
+	{
+		Variable symbol = new Variable (variable);
+		if (order != 1) return Power.reference (symbol, order);
+		return symbol;
+	}
+
+
+	// complexity management
+
+	/**
+	 * check for child nodes
+	 * @param factor source to check
+	 * @return TRUE when instance-of Factors
+	 */
+	public static boolean isMultiFactored (Factor factor)
+	{
+		return factor instanceof Factors;
+	}
+
+	/**
+	 * check for single child case
+	 * @param factors source to check
+	 * @return TRUE when child count is one
+	 */
+	public static boolean hasSingleChild (Factors factors)
+	{
+		return factors.size () == 1;
+	}
+
+	/**
+	 * get singleton child node
+	 * @param factors source to check
+	 * @return child node or NULL when not singleton
+	 */
+	public static Factor getSingleChild (Factors factors)
+	{
+		if ( hasSingleChild (factors) )
+			return factors.get (0);
+		else return null;
+	}
+
+	/**
+	 * promote single factors
+	 * @param factor a factor that may be a single factor set
+	 * @return the single factor where appropriate otherwise parameter factor
+	 */
+	public static Factor reduceSingle (Factor factor)
+	{
+		if (isMultiFactored (factor))
+		{
+			Factor child = getSingleChild ( (Factors) factor );
+			if ( child != null ) return child;
+		}
+		return factor;
+	}
+
+	/**
+	 * check for single grand-child
+	 * @param terms the source to check
+	 * @return TRUE if contains single grand-child
+	 */
+	public static boolean simpleReference (Sum terms)
+	{
+		Factor child = getSingleChild (terms);
+		if ( child == null || ! isMultiFactored (child) ) return false;
+		return hasSingleChild ( (Factors) child );
+	}
+
+	/**
+	 * treat factor as factor list when appropriate
+	 * @param factor the source node to check for children
+	 * @return NULL if not appropriate otherwise child list
+	 */
+	public static Factors getChildList (Factor factor)
+	{
+		if ( ! isMultiFactored (factor) ) return null;
+		else return (Factors) factor;
+	}
+
+
 	// display formatter
 
 	/**
@@ -139,17 +254,19 @@ public class Elements
 		int factorCount; StringBuffer buf;
 		if ((factorCount = factors.size ()) > 0)
 		{
-			buf = new StringBuffer ("( ")
+			buf = new StringBuffer ()
 				.append (factors.get (0).toString ());
 			for (int i = 1; i < factorCount; i++)
 			{
-				buf.append (" ")
-					.append (op).append (" ")
-					.append (factors.get (i).toString ());
+				buf.append (op).append (factors.get (i).toString ());
 			}
-			return buf.append (" )").toString ();
+			return buf.toString ();
 		}
 		return "";
+	}
+	public static String bracketedImage (Factors factors, String op)
+	{
+		return "( " + image (factors, op) + " )";
 	}
 
 
