05b6b2181f23b3c3df5e2684d193fcd3fbecd73c
Michael Druckman <spydrcom@gmail.com>
2023-03-07 14:33:11 -0800

    Update SeriesExpansion.java
    added expression tree lookup as step in processing EXPAND command

--- src/net/myorb/math/polynomial/algebra/SeriesExpansion.java
+++ src/net/myorb/math/polynomial/algebra/SeriesExpansion.java
@@ -3,6 +3,9 @@ package net.myorb.math.polynomial.algebra;
 
 import net.myorb.math.expressions.commands.CommandSequence;
 import net.myorb.math.expressions.evaluationstates.Environment;
+import net.myorb.math.expressions.evaluationstates.Subroutine;
+
+import net.myorb.data.notations.json.JsonPrettyPrinter;
 
 /**
  * command implementation for Series Expansion algorithm
@@ -26,8 +29,17 @@ public class SeriesExpansion <T>
 	public void performExpansion
 	(String functionName, CommandSequence tokens, int tokenPosition)
 	{
-		System.out.println (functionName + " - " + tokenPosition);
-		System.out.println (environment.getSymbolMap().get(functionName));
+		Subroutine<T> s = Subroutine.cast (environment.getSymbolMap ().get (functionName));
+		if (s == null) throw new RuntimeException ("Symbol is not a user defined function: " + functionName);
+
+		try { s.allowExpressionTree (); s.enableExpression (); }
+		catch (Exception e) { throw new RuntimeException ("Error building tree", e); }
+
+		try { JsonPrettyPrinter.sendTo (s.getExpression ().toJson (), System.out); }
+		catch (Exception e) { e.printStackTrace (); }
+
+		Elements.Equation eqn = RepresentationConversions.translate (s.getExpression ().toJson ());
+		System.out.println (eqn);
 	}
 
 
