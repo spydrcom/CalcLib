654f5c63c61bdbfaf344546604b8776b82580e2a
Michael Druckman <spydrcom@gmail.com>
2023-03-30 15:45:48 -0700

    Create InitialConditions.java
    definition of an object that is able to produce initial condition values for function
    intended to hold code for computation of values for differential equations

---
+++ src/net/myorb/math/polynomial/InitialConditions.java
@@ -0,0 +1,25 @@
+
+package net.myorb.math.polynomial;
+
+/**
+ * for polynomials that are solutions to differential equations
+ * - the solutions require values for the function Y intercept and rate of change at 0
+ * @param <T> type on which operations are to be executed
+ * @author Michael Druckman
+ */
+public interface InitialConditions <T>
+{
+
+	/**
+	 * function Y intercept
+	 * @return function at 0
+	 */
+	T getConstantTerm ();
+
+	/**
+	 * rate of change at 0
+	 * @return function derivative at 0
+	 */
+	T getLeadTerm ();
+
+}
