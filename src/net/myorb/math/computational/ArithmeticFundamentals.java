da984ca059327e1e7289c887f25720310ef7e7f4
Michael Druckman <spydrcom@gmail.com>
2023-04-15 14:33:36 -0700

    Update ArithmeticFundamentals.java
    Javadoc updates

--- src/net/myorb/math/computational/ArithmeticFundamentals.java
+++ src/net/myorb/math/computational/ArithmeticFundamentals.java
@@ -13,52 +13,189 @@ import net.myorb.data.abstractions.SpaceConversion;
 public class ArithmeticFundamentals
 {
 
+	/**
+	 * describe the atomic value
+	 */
 	public interface Scalar extends Comparable <Scalar>
 	{
 
+		/**
+		 * @return value as integer
+		 */
 		int intValue ();
+
+		/**
+		 * @return value as Double
+		 */
 		Double toDouble ();
 
+		/**
+		 * @return value treated as Java Number
+		 */
+		Number toNumber ();
+
+		/**
+		 * update this scalar to value taken from another
+		 * @param newValue value to copy
+		 */
 		void set (Scalar newValue);
 
+		/**
+		 * compare to a Double
+		 * @param value a Double value
+		 * @return TRUE when found equal
+		 */
 		boolean isEqualTo (Double value);
+
+		/**
+		 * compare to a Double
+		 * @param value a Double value
+		 * @return TRUE when found NOT equal
+		 */
 		boolean isNot (Double value);
+
+		/**
+		 * compare to another Scalar
+		 * @param with another Scalar value
+		 * @return TRUE when found equal
+		 */
 		boolean EQ (Scalar with);
 
+		/**
+		 * @return TRUE when greater than zero
+		 */
 		boolean isPositive ();
+
+		/**
+		 * @return TRUE when less than zero
+		 */
 		boolean isNegative ();
 
+		/**
+		 * sum of this with addend
+		 * @param addend the value to use as offset
+		 * @return sum of the two values
+		 */
 		Scalar plus (Scalar addend);
+
+		/**
+		 * product of this with factor
+		 * @param factor the value to use as scalar
+		 * @return product of the two values
+		 */
 		Scalar times (Scalar factor);
+
+		/**
+		 * exponentiation of this with integer
+		 * @param exponent the value to use as exponent
+		 * @return this raised to exponent
+		 */
 		Scalar pow (int exponent);
+
+		/**
+		 * negate this value
+		 * @return negative value of this
+		 */
 		Scalar negated ();
 
 	}
 
+	/**
+	 * provide conversion between generic and scalar
+	 * @param <T> the data type
+	 */
 	public interface Conversions <T>
 		extends SpaceConversion <Scalar>, java.util.Comparator <Scalar>
 	{
+
+		 /**
+		 * convert to scalar
+		 * @param value a generic value
+		 * @return the equivalent scalar representation
+		 */
 		Scalar toScalar (T value);
+
+		/**
+		 * translate representation to Number
+		 * - this will use the algorithm supplied in the type manager
+		 * @param value a Scalar representation of a value
+		 * @return a machine implemented type
+		 */
+		Number toNumber (Scalar value);
+
+		/**
+		 * convert to generic
+		 * @param value scalar representation of the value
+		 * @return the equivalent generic representation
+		 */
 		T convertedFrom (Scalar value);
+
+		/**
+		 * @return the manager for the data type used
+		 */
 		ExpressionSpaceManager <T> getManager ();
 
-		Scalar fromInt (Integer D);
-		Scalar fromDouble (Double D);
-		Scalar fromText (String V);
+		/**
+		 * @param I an integer value
+		 * @return the equivalent scalar representation
+		 */
+		Scalar fromInt (Integer I);
 
+		/**
+		 * @param D an integer value
+		 * @return the equivalent scalar representation
+		 */
+		Scalar fromDouble (Double D);
+
+		/**
+		 * evaluate an equation
+		 *  and give the scalar representation of the value
+		 * @param text the text of a symbolic equation
+		 * @return the computed scalar representation
+		 */
+		Scalar fromText (String text);
+
+		/**
+		 * @return scalar representation of 1
+		 */
 		Scalar getOne ();
+
+		/**
+		 * @return scalar representation of -1
+		 */
 		Scalar getNegOne ();
+
+		/**
+		 * @return scalar representation of 0
+		 */
 		Scalar getZero ();
 	}
+
+	/**
+	 * get a Converter based on Expression Space Manager for type
+	 * @param manager Expression Space Manager for the data type
+	 * @return a Converter for the specified type
+	 * @param <T> the data type
+	 */
 	public static <T> Conversions <T> getConverter
 		( ExpressionSpaceManager <T> manager )
 	{ return new Converter <> (manager); }
 
+	/**
+	 * scale value with operand
+	 * @param value the value being updated
+	 * @param operand the scaling value to multiply
+	 */
 	public static void timesEquals (Scalar value, Scalar operand)
 	{
 		value.set (value.times (operand));
 	}
 
+	/**
+	 * increment value with operand
+	 * @param value the value being updated
+	 * @param operand the offset value to add
+	 */
 	public static void plusEquals (Scalar value, Scalar operand)
 	{
 		value.set (value.plus (operand));
@@ -83,10 +220,20 @@ abstract class Core <T>
 	protected ExpressionSpaceManager <T> manager;
 	protected T ZERO;
 
+	/**
+	 * retrieve generic value from Scalar
+	 * @param S the Scalar representation
+	 * @return the generic value
+	 */
 	@SuppressWarnings("unchecked")
 	T getValue (ArithmeticFundamentals.Scalar S)
 	{ return ( (ArithmeticScalar <T>) S ).value; }
 
+	/**
+	 * wrap generic value as scalar
+	 * @param value the generic value
+	 * @return Scalar representation
+	 */
 	ArithmeticScalar <T> newValue (T value)
 	{ return new ArithmeticScalar <> (value, conversions); }
 
@@ -133,6 +280,11 @@ class Converter <T> extends Core <T> implements ArithmeticFundamentals.Conversio
 	 * @see net.myorb.data.abstractions.SpaceConversion#convertToDouble(java.lang.Object)
 	 */
 	public Double convertToDouble (ArithmeticFundamentals.Scalar value) { return manager.convertToDouble (getValue (value)); }
+
+	/* (non-Javadoc)
+	 * @see net.myorb.math.computational.ArithmeticFundamentals.Conversions#toNumber(net.myorb.math.computational.ArithmeticFundamentals.Scalar)
+	 */
+	public Number toNumber (ArithmeticFundamentals.Scalar value) { return manager.toNumber (getValue (value)); }
 
 	/* (non-Javadoc)
 	 * @see net.myorb.math.computational.ArithmeticFundamentals.Conversions#getZero()
