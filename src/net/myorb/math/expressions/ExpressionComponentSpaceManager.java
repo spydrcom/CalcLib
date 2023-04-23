e45b0e8c0b5f4ac43740d75107aa5060daa0b0af
Michael Druckman <spydrcom@gmail.com>
2023-04-23 16:34:37 -0700

    Update ExpressionComponentSpaceManager.java
    component interface separated into new object
    this manager extends component object now

--- src/net/myorb/math/expressions/ExpressionComponentSpaceManager.java
+++ src/net/myorb/math/expressions/ExpressionComponentSpaceManager.java
@@ -6,34 +6,15 @@ import net.myorb.math.expressions.charting.MultiDimensionalUtilities;
 import net.myorb.charting.DisplayGraphTypes;
 
 /**
- * provide for working with values that break into components
+ * treatments for working with values that break into components
  * @param <T> type on which operations are to be executed
  * @author Michael Druckman
  */
 public interface ExpressionComponentSpaceManager<T>
-	extends ExpressionSpaceManager <T>, MultiDimensionalUtilities.ContextProperties
+	extends ExpressionSpaceManager <T>, ComponentAccess <T>,
+		MultiDimensionalUtilities.ContextProperties
 {
 
-	/**
-	 * get the number of components making this structure
-	 * @return the number of components
-	 */
-	int getComponentCount ();
-
-	/**
-	 * construct the generic structure from components
-	 * @param components the components to use for the process
-	 * @return a new structured generic item
-	 */
-	T construct (double... components);
-
-	/**
-	 * evaluate one component of the value
-	 * @param value the value being evaluated
-	 * @param componentNumber the ID of component (ordinal index)
-	 * @return an evaluation of the component
-	 */
-	double component (T value, int componentNumber);
 
 	/**
 	 * distance of a value from the origin
