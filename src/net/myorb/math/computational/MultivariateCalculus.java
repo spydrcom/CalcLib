22c72ef05e24e4042418616c618ff4a748ddc7cb
Michael Druckman <spydrcom@gmail.com>
2023-04-25 13:19:17 -0700

    Update MultivariateCalculus.java
    refactor of processVectorOperation
    MultivariateOperator class moved to Gradients source file
    VectorOperator class structure simplified
    Environment moved to base class
    execute methods made NULL

--- src/net/myorb/math/computational/MultivariateCalculus.java
+++ src/net/myorb/math/computational/MultivariateCalculus.java
@@ -1,20 +1,14 @@
 
 package net.myorb.math.computational;
 
-import net.myorb.math.computational.multivariate.*;
-
-import net.myorb.math.expressions.SymbolMap.Operation;
-import net.myorb.math.expressions.SymbolMap.SymbolType;
-
-import net.myorb.math.expressions.evaluationstates.Environment;
-
-import net.myorb.math.expressions.SymbolMap.MultivariateOperator;
-import net.myorb.math.expressions.symbols.AbstractUnaryOperator;
-
-import net.myorb.math.expressions.gui.rendering.MathMarkupNodes;
 import net.myorb.math.expressions.gui.rendering.NodeFormatting;
+import net.myorb.math.expressions.gui.rendering.MathMarkupNodes;
 
-import net.myorb.math.expressions.ValueManager.GenericValue;
+import net.myorb.math.computational.multivariate.VectorOperations;
+
+import net.myorb.math.expressions.symbols.AbstractUnaryOperator;
+import net.myorb.math.expressions.evaluationstates.Environment;
+import net.myorb.math.expressions.ValueManager;
 
 /**
  * implementations of algorithms computing Multivariate calculus operations
@@ -25,73 +19,40 @@ public class MultivariateCalculus <T> extends VectorOperations <T>
 {
 
 
+	public MultivariateCalculus
+	(Environment <T> environment)
+	{ super (environment); }
+
+
 	/**
 	 * abstract base class for operator implementation objects
 	 */
 	public static abstract class VectorOperator extends AbstractUnaryOperator
 			implements CalculusMarkers.CalculusMetadata
 	{
-		public VectorOperator
+		public <T> VectorOperator
 		(
 			String name, int precedence,
-			CalculusMarkers.CalculusMarkerTypes type
+			CalculusMarkers.CalculusMarkerTypes type,
+			Environment <T> environment
 		)
-		{ super (name, precedence); this.type = type; }
+		{ super (name, precedence); this.type = type; this.environment = environment; }
+		protected CalculusMarkers.CalculusMarkerTypes type;
+
+		/* (non-Javadoc)
+		 * @see net.myorb.math.expressions.SymbolMap.ExecutableUnaryOperator#execute(net.myorb.math.expressions.ValueManager.GenericValue)
+		 */
+		public ValueManager.GenericValue execute (ValueManager.GenericValue parameter) { return null; }
 
 		/* (non-Javadoc)
 		 * @see net.myorb.math.computational.CalculusMarkers.CalculusMetadata#typeOfOperation()
 		 */
 		public CalculusMarkers.CalculusMarkerTypes typeOfOperation () { return type; }
-		public Environment <?> getEnvironment () { return null; }
-		protected CalculusMarkers.CalculusMarkerTypes type;
-	}
 
-
-	public static Operation processVectorOperation (VectorOperator VO, Operation function)
-	{
-		return new MultivariateOperator ()
-		{
-			OperationContext context = new OperationContext
-				(
-					function.getName (), new OperationMetadata (VO, function)
-				);
-
-			/* (non-Javadoc)
-			 * @see net.myorb.math.expressions.SymbolMap.ParameterizedFunction#markupForDisplay(java.lang.String, java.lang.String, net.myorb.math.expressions.gui.rendering.NodeFormatting)
-			 */
-			public String markupForDisplay
-			(String operator, String parameters, NodeFormatting using)
-			{ return null; }
-
-			/* (non-Javadoc)
-			 * @see net.myorb.math.expressions.SymbolMap.ExecutableUnaryOperator#execute(net.myorb.math.expressions.ValueManager.GenericValue)
-			 */
-			public GenericValue execute (GenericValue parameter)
-			{
-//				System.out.println (parameter);
-//				System.out.println ( "DATA " + parameter.getClass ().getCanonicalName () );
-
-				context.dump (parameter);
-				context.setEvaluationPoint (parameter);
-				return context.getFunction ().execute
-				( context.getEvaluationPoint () );
-			}
-
-			public String getName () { return context.getFunction ().getName (); }
-			public SymbolType getSymbolType () { return SymbolType.PARAMETERIZED; }
-			
-			/* (non-Javadoc)
-			 * @see net.myorb.math.expressions.SymbolMap.ParameterizedFunction#getParameterList()
-			 */
-			public String getParameterList () { return null; }
-			public String formatPretty () { return null; }
-
-			/* (non-Javadoc)
-			 * @see net.myorb.math.expressions.SymbolMap.Operation#getPrecedence()
-			 */
-			public int getPrecedence () { return 99; }	// must be highest possible
-
-		};
+		@SuppressWarnings("unchecked")
+		public <T> Environment <T> getEnvironment ()
+		{ return (Environment <T>) environment; }
+		protected Environment <?> environment;
 	}
 
 
@@ -112,9 +73,6 @@ public class MultivariateCalculus <T> extends VectorOperations <T>
 		return op  + MathMarkupNodes.space ("2") + operand;
 	}
 	static final String NABLA = "\u2207";
-
-
-	public MultivariateCalculus (Environment <T> environment) { super (environment); }
 
 
 }
