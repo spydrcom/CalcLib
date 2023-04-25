d1d007b76bc554e8ccc9bea0483c301cf07e7fd4
Michael Druckman <spydrcom@gmail.com>
2023-04-25 15:31:37 -0700

    Create OperationMetadata.java
    refactor of context and meta-data

---
+++ src/net/myorb/math/computational/multivariate/OperationMetadata.java
@@ -0,0 +1,49 @@
+
+package net.myorb.math.computational.multivariate;
+
+import net.myorb.math.computational.MultivariateCalculus;
+
+import net.myorb.math.expressions.ValueManager.Metadata;
+import net.myorb.math.expressions.SymbolMap.Operation;
+
+/**
+ * meta-data collected when the operation was invoked
+ * @param <T> the data type used by the function
+ * @author Michael Druckman
+ */
+public class OperationMetadata implements Metadata
+{
+
+
+	public OperationMetadata (Operation op, Operation target)
+	{
+		this.setOp ( (MultivariateCalculus.VectorOperator) op );
+		this.setTarget (target);
+	}
+
+
+	public void setOp (MultivariateCalculus.VectorOperator op) { this.op = op; }
+	public MultivariateCalculus.VectorOperator getOperation () { return this.op; }
+	public MultivariateCalculus.VectorOperator getOp () { return op; }
+	private MultivariateCalculus.VectorOperator op;
+
+
+	public void setTarget
+		(Operation target) { this.target = target; }
+	public Operation getTarget () { return this.target; }
+	private Operation target = null;
+
+
+	public void setEvaluationPoint
+	(FunctionCoordinates.Coordinates evaluationPoint) { this.evaluationPoint = evaluationPoint; }
+	public FunctionCoordinates.Coordinates getEvaluationPoint () { return evaluationPoint; }
+	private FunctionCoordinates.Coordinates evaluationPoint;
+
+
+	public int getParameters () { return parameters; }
+	public void setParameters (int parameters) { this.parameters = parameters; }
+	private int parameters = 1;
+
+
+}
+
