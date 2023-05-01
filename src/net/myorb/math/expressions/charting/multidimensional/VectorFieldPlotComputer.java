88bd8ebfd33960cbd091afb4382deb6f9103ceca
Michael Druckman <spydrcom@gmail.com>
2023-05-01 12:03:59 -0700

    Update VectorFieldPlotComputer.java
    data collection for plot points for direction indicators completed
    TRACE being used for POC
    plot requests TBD

--- src/net/myorb/math/expressions/charting/multidimensional/VectorFieldPlotComputer.java
+++ src/net/myorb/math/expressions/charting/multidimensional/VectorFieldPlotComputer.java
@@ -6,8 +6,6 @@ import net.myorb.math.expressions.charting.ContourPlotProperties;
 import net.myorb.charting.DisplayGraphTypes;
 import net.myorb.charting.Histogram;
 
-import java.util.*;
-
 /**
  * a PlotComputer that captures both magnitude and direction
  * @author Michael Druckman
@@ -15,9 +13,11 @@ import java.util.*;
 public class VectorFieldPlotComputer extends BruteForcePlotComputer
 {
 
+
 	public VectorFieldPlotComputer (ContourPlotProperties proprties, int vectorCount)
 	{ super (proprties); this.vectorCount = vectorCount; }
 	protected int vectorCount;
+
 
 	/* (non-Javadoc)
 	 * @see net.myorb.math.expressions.charting.multidimensional.BruteForcePlotComputer#compute(net.myorb.charting.DisplayGraphTypes.ContourPlotDescriptor, int, net.myorb.charting.DisplayGraphTypes.Point[], java.lang.Object[], net.myorb.charting.Histogram)
@@ -28,34 +28,82 @@ public class VectorFieldPlotComputer extends BruteForcePlotComputer
 			DisplayGraphTypes.Point[] points, Object[] range, Histogram histogram
 		)
 	{
-		this.axisSize = pointsPerAxis;
-		this.separation = pointsPerAxis / vectorCount;
 		this.angle = new double [pointsPerAxis * pointsPerAxis];
-		super.compute (descriptor, pointsPerAxis, points, range, histogram);
-		displayVectors ();
-	}
-	int separation, axisSize;
 
-	void displayVectors ()
+		super.compute (descriptor, pointsPerAxis, points, range, histogram);
+
+		DisplayGraphTypes.VectorField.Locations
+			vectorPoints = new DisplayGraphTypes.VectorField.Locations ();
+		collectVectorPoints (pointsPerAxis, vectorPoints);
+	}
+
+
+	/**
+	 * collect necessary data for plot of a vector field
+	 * @param axisSize the pixel count specified as edge size
+	 * @param vectorPoints the point list for display
+	 */
+	void collectVectorPoints
+		(int axisSize, DisplayGraphTypes.VectorField.Locations vectorPoints)
+	{ computeDisplayFactors (axisSize); describeVectorField (vectorPoints); }
+
+
+	/**
+	 * compute pixel location for direction displays given vector count
+	 * @param axisSize the pixel count specified as edge size
+	 */
+	void computeDisplayFactors (int axisSize)
+	{
+		this.separation = axisSize / vectorCount;
+		this.blockSize	= axisSize * separation;
+		this.halfBlock	= blockSize / 2;
+	}
+	protected int separation, blockSize, halfBlock;
+
+
+	/**
+	 * field data collection
+	 * - data collection done for graphical interface
+	 * @param plotPoints the collection of points to be added to the plot
+	 */
+	void describeVectorField (DisplayGraphTypes.VectorField.Locations plotPoints)
 	{
 		try
 		{
-			int next = 0;
-			while (next < angle.length)
+			for
+				(
+					int next = halfBlock;
+						next < angle.length;
+						next += blockSize
+				)
 			{
-				next += separation * axisSize;
-				List <Double> row = new ArrayList <Double> ();
-				for (int n = axisSize; n > 0; n-=separation)
+				DisplayGraphTypes.VectorField.Locations
+					row = new DisplayGraphTypes.VectorField.Locations ();
+
+				for
+					(
+						int P = next + this.separation / 2,
+							n = vectorCount; n > 0; n--
+					)
 				{
-					next += separation;
-					row.add ( angle [next] );
+					row.add
+					(
+						new DisplayGraphTypes.VectorField
+							(
+								points[P], (Integer)range[P], angle[P]
+							)
+					);
+					P += separation;
 				}
-				System.out.println ("next=" + next + " : " + row);
+
+				if (TRACE) System.out.println ("next=" + next + " : " + row);
+				plotPoints.addAll (row);
 			}
 		}
 		catch (Exception e) { e.printStackTrace (); }
-		System.out.println ("display complete");
 	}
+	boolean TRACE = true;
+
 
 	/* (non-Javadoc)
 	 * @see net.myorb.math.expressions.charting.PlotMatrixTraversal#processPoint(double, double)
@@ -69,3 +117,4 @@ public class VectorFieldPlotComputer extends BruteForcePlotComputer
 
 
 }
+
