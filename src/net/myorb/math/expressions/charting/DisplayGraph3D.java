52f89d404fafb97320f5a369eed543dba6b3037b
Michael Druckman <spydrcom@gmail.com>
2023-05-01 17:31:41 -0700

    Update DisplayGraph3D.java
    complete refactor of DisplayGraph class
    sections moved into groups with common functionalities
    extensive increase in commenting to document algorithms
    some functionality extracted into smaller methods
    logic layer sizes increase but code reuse improves
    vector field plotting has become much improved

--- src/net/myorb/math/expressions/charting/DisplayGraph3D.java
+++ src/net/myorb/math/expressions/charting/DisplayGraph3D.java
@@ -5,6 +5,7 @@ import net.myorb.math.expressions.charting.PlotComputers;
 import net.myorb.math.expressions.charting.PlotComputers.TransformResultsCollection;
 
 import net.myorb.charting.ColorSelection;
+import net.myorb.charting.DisplayGraphTypes;
 import net.myorb.charting.Histogram;
 
 import net.myorb.rinearn.SurfacePlotter;
@@ -29,6 +30,8 @@ public class DisplayGraph3D extends DisplayGraph
 {
 
 
+	// plot data from file
+
 	/**
 	 * Display tabular plot with data taken from a file
 	 * @param filepath path to source of plot data
@@ -39,6 +42,8 @@ public class DisplayGraph3D extends DisplayGraph
 		plotter.plot (filepath);
 	}
 
+
+	// 3D mesh plot algorithms
 
 	/**
 	 * build the plot data as mesh
@@ -65,84 +70,95 @@ public class DisplayGraph3D extends DisplayGraph
 	}
 
 
+	// vector field plot algorithms
+
+	/**
+	 * build the plot data as Vector Field
+	 * @param proprties description of the transform
+	 * @param description a description identifying the plot
+	 */
+	public static void plotVectorField
+		(ContourPlotProperties proprties, String description)
+	{
+		// contour plot shows vector magnitudes
+		BufferedImage image = buildContourImage (proprties);
+
+		Plot3DVectorField <?> plotter = ( Plot3DVectorField <?> ) proprties;
+		// the vector field version of the properties contains direction data for display
+		DisplayGraphTypes.VectorField.Locations plotPoints = plotter.getVectorPoints ();
+		if (TRACE) show (plotPoints); // for debugging use
+
+		offAxisVectorFieldPlot
+		(
+			plotPoints,
+			proprties.getPointsSize (),
+			proprties.getLowCorner (),
+			proprties.getEdgeSize (),
+			image
+		);
+
+		// use common display and tracking layers
+		showImageAsComponent (description, proprties, image);
+		addTrackingFor (proprties, description);
+	}
+
+	/**
+	 * add vector direction indicators to contour plot
+	 * @param plotPoints Vector Field Locations collected for plot
+	 * @param pointSize the computed appropriate point size for displays
+	 * @param lowCorner the low corner coordinates use for establishing context
+	 * @param edgeSize the number of pixels in each edge of the plot
+	 * @param image the Java buffered image object to use
+	 */
+	public static void offAxisVectorFieldPlot
+		(
+			DisplayGraphTypes.VectorField.Locations plotPoints, 
+			int pointSize, Point lowCorner, double edgeSize,
+			BufferedImage image
+		)
+	{
+		int width = image.getWidth ();
+		Point scaledPoint = new Point ();
+		DisplayGraphTypes.VectorField point;
+		Graphics2D g = image.createGraphics ();
+		double pixelsPer = width / edgeSize;
+	
+		for (int i = 0; i < plotPoints.size (); i++)
+	    {
+			point = plotPoints.get (i);
+			// the point gets scaled into display coordinates
+			scale ( point, lowCorner, width, pixelsPer, scaledPoint );
+			// the point is redrawn and the direction indicator is added
+	    	markDirection ( scaledPoint, point.getAngle (), pointSize+1, g );
+	    }
+	
+	    g.dispose ();
+	}
+
+	/**
+	 * @param plotPoints data to be dumped in trace
+	 */
+	static void show (DisplayGraphTypes.VectorField.Locations plotPoints)
+	{ for (DisplayGraphTypes.VectorField point : plotPoints) System.out.println (point); }
+	static boolean TRACE = false;
+
+
+	// most basic contour plot algorithms
+
 	/**
 	 * build the plot data as contour
 	 * @param proprties description of the transform
 	 * @param description a description identifying the plot
+	 * @return contour image
 	 */
-	public static void plotContour
+	public static BufferedImage plotContour
 		(ContourPlotProperties proprties, String description)
 	{
-		int pointSize = proprties.getPointsSize ();
-		int pointsPerAxis = proprties.getPointsPerAxis ();
-
-		Point[] points = new Point[pointsPerAxis * pointsPerAxis];
-		Object[] range = new Object[pointsPerAxis * pointsPerAxis];
-
-		compute (proprties, pointsPerAxis, points, range); //TODO: fix properties (Y)
-
-		offAxisContourPlot
-		(
-			range, points, pointSize,
-			proprties.getLowCorner (), proprties.getEdgeSize (),
-			description, new OffAxisHandler3D (proprties, pointsPerAxis, pointSize)
-		);
-
+		BufferedImage image = buildContourImage (proprties);
+		showImageAsComponent (description, proprties, image);
 		addTrackingFor (proprties, description);
+		return image;
 	}
-	public static void addTrackingFor (ContourPlotProperties proprties, String description)
-	{ Tracking.getInstance (false).add (proprties, description, new ContourPlotProperties (proprties)); }
-	public static void addTrackingFor (Map<String,Object> properties, String description, String type)
-	{ Tracking.getInstance (true).add (description, properties); }
-
-
-	/**
-	 * fill image with points
-	 * @param colors the color for each point
-	 * @param points the points of the X/Y axis
-	 * @param pointSize the size of each point
-	 * @param scaleFactors computed factors
-	 * @param image the image object
-	 */
-	public static void countourPlot
-	(Colors colors, Point.Series points, int pointSize, Point scaleFactors, BufferedImage image)
-	{
-		add2Percent (scaleFactors);
-		Point.Series scaled = scale4Quad (points, scaleFactors, image.getWidth ());
-		Graphics2D g = image.createGraphics ();
-	    
-	    for (int i = 0; i < scaled.size (); i++)
-	    {
-	    	markPoint (scaled.get (i), colors.get (i), pointSize, g);
-	    }
-
-	    g.dispose ();
-	}
-
-
-	/**
-	 * generate single point with fill operation
-	 * @param p the x,y point at which to mark plot
-	 * @param c the color to be used for the mark
-	 * @param size the size of point to be drawn
-	 * @param g the object in which too draw
-	 */
-	public static void markPoint (Point p, Color c, int size, Graphics2D g)
-	{
-		g.setColor (c); g.fillOval ((int)p.x, (int)p.y, size, size);
-	}
-
-
-	/**
-	 * compute pixels in a point given plot size
-	 * @param usingAxisSize the pixels along each axis
-	 * @return the computed point size
-	 */
-	public static int appropriatePointSize (int usingAxisSize)
-	{
-		return 2 + DEFAULT_PLOT_SIZE / usingAxisSize;
-	}
-
 
 	/**
 	 * 3D plot with color as the third axis
@@ -161,11 +177,8 @@ public class DisplayGraph3D extends DisplayGraph
 		Point scalingFactors = scale (funcPlot);
 		BufferedImage image = chartBufferedImage (DEFAULT_PLOT_SIZE);
 		countourPlot (colors, funcPlot, pointSize, scalingFactors, image);
-		JComponent c = showImage (image, title, mouse);
-		mouse.setWidth (image.getWidth ());
-		mouse.set (c);
+		showImageAsComponent (image, title, mouse);
 	}
-
 
 	/**
 	 * produce contour plot with
@@ -175,23 +188,19 @@ public class DisplayGraph3D extends DisplayGraph
 	 * @param pointSize the size of each point on plot
 	 * @param lowCorner X/Y for low left corner
 	 * @param edgeSize length of each axis
-	 * @param title for the frame
-	 * @param mouse the handler
+	 * @return new image
 	 */
-	public static void offAxisContourPlot
+	public static BufferedImage offAxisContourPlot
 		(
 			Object[] range, Point[] domain,
-			int pointSize, Point lowCorner, double edgeSize,
-			String title, MouseMotionHandler mouse
+			int pointSize, Point lowCorner,
+			double edgeSize
 		)
 	{
 		BufferedImage image = createBufferedImage (DEFAULT_PLOT_SIZE-MARGIN);
 		offAxisContourPlot (range, domain, pointSize, lowCorner, edgeSize, image);
-		JComponent c = showImage (image, title, mouse);
-		mouse.setWidth (image.getWidth ());
-		mouse.set (c);
+		return image;
 	}
-
 
 	/**
 	 * construct graphics and add to image
@@ -223,6 +232,176 @@ public class DisplayGraph3D extends DisplayGraph
 	}
 
 
+	// low level plot operations
+
+	/**
+	 * construct the contour image
+	 * @param proprties description of the transform
+	 * @return contour image
+	 */
+	public static BufferedImage buildContourImage
+		  (ContourPlotProperties proprties)
+	{
+		int pointsPerAxis = proprties.getPointsPerAxis ();
+
+		Point[] points = new Point[pointsPerAxis * pointsPerAxis];
+		Object[] range = new Object[pointsPerAxis * pointsPerAxis];
+
+		compute (proprties, pointsPerAxis, points, range);
+
+		return offAxisContourPlot
+		(
+			range, points, proprties.getPointsSize (),
+			proprties.getLowCorner (), proprties.getEdgeSize ()
+		);
+	}
+	public static void addTrackingFor (ContourPlotProperties proprties, String description)
+	{ Tracking.getInstance (false).add (proprties, description, new ContourPlotProperties (proprties)); }
+	public static void addTrackingFor (Map<String,Object> properties, String description, String type)
+	{ Tracking.getInstance (true).add (description, properties); }
+
+	/**
+	 * fill image with points
+	 * @param colors the color for each point
+	 * @param points the points of the X/Y axis
+	 * @param pointSize the size of each point
+	 * @param scaleFactors computed factors
+	 * @param image the image object
+	 */
+	public static void countourPlot
+		(
+			Colors colors, Point.Series points,
+			int pointSize, Point scaleFactors,
+			BufferedImage image
+		)
+	{
+		add2Percent (scaleFactors);
+		Point.Series scaled = scale4Quad (points, scaleFactors, image.getWidth ());
+		Graphics2D g = image.createGraphics ();
+	    
+	    for (int i = 0; i < scaled.size (); i++)
+	    {
+	    	markPoint (scaled.get (i), colors.get (i), pointSize, g);
+	    }
+
+	    g.dispose ();
+	}
+
+
+	// graphics primitives
+
+	/**
+	 * plot direction indicator for vector points
+	 * @param scaledPoint the point scaled to graphics coordinates
+	 * @param angle the angle the vector should display direction at
+	 * @param pointSize the chosen point size for the plot
+	 * @param g the 2D graphics object
+	 */
+	public static void markDirection
+		(
+			Point scaledPoint, double angle,
+			int pointSize, Graphics2D g
+		)
+	{
+		Point adjusted = adjustedPoint
+			( scaledPoint, pointSize / 2 );
+		Point end = endPoint ( adjusted, angle );
+		// the point is redrawn as WHITE to make it stand out
+		markPoint ( scaledPoint, Color.WHITE, pointSize, g );
+
+		g.drawLine // a line extending in the vector direction
+		(
+			(int) adjusted.x, (int) adjusted.y,
+			(int) end.x, (int) end.y
+		);
+	}
+
+	/**
+	 * adjust for point render shifting center
+	 * @param scaledPoint the coordinates after scaling
+	 * @param adjustment the adjustment to compensate for render
+	 * @return the adjusted coordinates for the point
+	 */
+	public static Point adjustedPoint
+		(Point scaledPoint, double adjustment)
+	{
+		int X = (int) (scaledPoint.x + adjustment),
+			Y = (int) (scaledPoint.y + adjustment);
+		return new Point (X, Y);
+	}
+
+	/**
+	 * compute end of vector
+	 * @param P the points starting the vector
+	 * @param angle the direction component of the vector
+	 * @return the coordinates for the end of vector
+	 */
+	public static Point endPoint (Point P, double angle)
+	{
+		double S = VACTOR_DISPLAY_LENGTH * Math.sin (angle),
+			C = VACTOR_DISPLAY_LENGTH * Math.cos (angle);
+		return new Point (P.x + S, P.y + C);
+	}
+	static int VACTOR_DISPLAY_LENGTH = 10;
+
+	/**
+	 * generate single point with fill operation
+	 * @param p the x,y point at which to mark plot
+	 * @param c the color to be used for the mark
+	 * @param size the size of point to be drawn
+	 * @param g the object in which too draw
+	 */
+	public static void markPoint (Point p, Color c, int size, Graphics2D g)
+	{
+		g.setColor (c); g.fillOval ( (int) p.x, (int) p.y, size, size );
+	}
+
+
+	// wrap image into component for display
+
+	/**
+	 * build component for plot image
+	 * @param title a title to display with the image
+	 * @param proprties description of the transform
+	 * @param image the buffered image with the plot
+	 */
+	public static void showImageAsComponent
+		(
+			String title,
+			ContourPlotProperties proprties,
+			BufferedImage image
+		)
+	{
+		showImageAsComponent
+		(
+			image, title,
+
+			new OffAxisHandler3D
+			(
+				proprties,
+				proprties.getPointsPerAxis (),
+				proprties.getPointsSize ()
+			)
+		);
+	}
+
+	/**
+	 * build component for plot image
+	 * @param image the buffered image with the plot
+	 * @param title a title to display with the image
+	 * @param mouse the mouse handler to attach
+	 */
+	public static void showImageAsComponent
+	(BufferedImage image, String title, MouseMotionHandler mouse)
+	{
+		JComponent c = showImage (image, title, mouse);
+		mouse.setWidth (image.getWidth ());
+		mouse.set (c);
+	}
+
+
+	// compute function transform of parameters from plane
+
 	/**
 	 * transform domain to range
 	 * @param descriptor the descriptor for the transform
@@ -237,7 +416,6 @@ public class DisplayGraph3D extends DisplayGraph
 			Object[] range
 		)
 	{
-		//TODO: fix
 		if (DUMP) dump (descriptor);
 		Histogram histogram = new Histogram ();
 		descriptor.getPlotComputer ().computeRange
@@ -248,8 +426,21 @@ public class DisplayGraph3D extends DisplayGraph
 	}
 	static void dump (ContourPlotDescriptor descriptor)
 	{ System.out.println ("COMPUTE: " + descriptor); System.out.println (); }
-	static boolean showHistogramBarChart =  false, showHistogramPlot = false, DUMP = false; //TODO: fix
+	static boolean showHistogramBarChart =  false, showHistogramPlot = false, DUMP = false;
 	static String HISTOGRAM_TITLE = "Display Spectrum";
+
+
+	// primitive computations
+
+	/**
+	 * compute pixels in a point given plot size
+	 * @param usingAxisSize the pixels along each axis
+	 * @return the computed point size
+	 */
+	public static int appropriatePointSize (int usingAxisSize)
+	{
+		return 2 + DEFAULT_PLOT_SIZE / usingAxisSize;
+	}
 
 
 }
