27f309339a91b378147ebaec3b41bd3545549f90
Michael Druckman <spydrcom@gmail.com>
2023-05-05 14:59:42 -0700

    Update OperationMetadata.java
    minor comment additions

--- src/net/myorb/math/computational/multivariate/OperationMetadata.java
+++ src/net/myorb/math/computational/multivariate/OperationMetadata.java
@@ -2,7 +2,6 @@
 package net.myorb.math.computational.multivariate;
 
 import net.myorb.math.computational.MultivariateCalculus;
-
 import net.myorb.math.expressions.ValueManager.Metadata;
 import net.myorb.math.expressions.SymbolMap.Operation;
 
@@ -21,24 +20,37 @@ public class OperationMetadata implements Metadata
 	}
 
 
+	/**
+	 * capture meta-data for execution of vector operations
+	 * - calculus markers are assigned to the operation extension
+	 * @param op the operation description of the vector operation being executed
+	 */
 	public void setOp (MultivariateCalculus.VectorOperator op) { this.op = op; }
 	public MultivariateCalculus.VectorOperator getOp () { return op; }
 	private MultivariateCalculus.VectorOperator op;
 
 
-	public void setTarget
-		(Operation target) { this.target = target; }
+	/**
+	 * @param target the operation describing the function being evaluated
+	 */
+	public void setTarget (Operation target) { this.target = target; }
 	public Operation getTarget () { return this.target; }
 	private Operation target = null;
 
 
-	public void setEvaluationPoint
-	(FunctionCoordinates.Coordinates evaluationPoint)
+	/**
+	 * capture the point at which the evaluation was done
+	 * @param evaluationPoint the coordinates of the evaluation point
+	 */
+	public void setEvaluationPoint (FunctionCoordinates.Coordinates evaluationPoint)
 	{ this.evaluationPoint = evaluationPoint; this.setParameters (evaluationPoint.size ()); }
 	public FunctionCoordinates.Coordinates getEvaluationPoint () { return evaluationPoint; }
 	private FunctionCoordinates.Coordinates evaluationPoint;
 
 
+	/**
+	 * @return the count of parameters in the invocation
+	 */
 	public int getParameters () { return parameters; }
 	public void setParameters (int parameters) { this.parameters = parameters; }
 	private int parameters = 1;
