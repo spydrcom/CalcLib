769160dd67c83f3298b1d031731b125b034ac7be
Michael Druckman <spydrcom@gmail.com>
2023-05-17 17:19:37 -0700

    Update LegendDisplay.java
    refactor of legend construction layers for broader use
    this is still a dirty little HACK, not cleaned up yet
    small changes to swing component connections
    a small bit better looking
    STILL TBD

--- src/net/myorb/math/expressions/charting/LegendDisplay.java
+++ src/net/myorb/math/expressions/charting/LegendDisplay.java
@@ -3,29 +3,46 @@ package net.myorb.math.expressions.charting;
 
 import net.myorb.charting.DisplayGraphTypes;
 
-import net.myorb.gui.components.SimpleScreenIO.*;
+import net.myorb.gui.components.SimpleScreenIO.Widget;
+import net.myorb.gui.components.SimpleScreenIO.Panel;
 import net.myorb.gui.components.SimpleScreenIO;
 
 import javax.swing.*;
+import java.awt.*;
 
-import java.awt.Color;
-import java.awt.GridLayout;
-
+/**
+ * swing structure for Legend display
+ * @author Michael Druckman
+ */
 public class LegendDisplay
 {
 
+	/**
+	 * @param legend pre-built components to be shown
+	 */
 	public static void show (DisplayGraphTypes.LegendWidgets legend)
 	{
-		Panel P = new Panel ();
+		JLabel L;
+		Panel widget = new Panel ();
+		JPanel inner, P = new JPanel ();
 		P.setLayout ( new GridLayout ( 0, 1 ) );
 		P.setBackground (Color.WHITE);
 
 		Widget [] widgets = legend.toArray (EMPTY);
 		for (int n = widgets.length-1; n >= 0; n--)
 		{
-			P.add ( (JLabel) widgets [n] );
+			( inner = new Panel () )
+				.add ( L = (JLabel) widgets [n] );
+			inner.setBackground ( L.getForeground () );
+			L.setPreferredSize ( new Dimension ( 100, 50 ) );
+			P.add ( inner, JPanel.CENTER_ALIGNMENT );
+			L.setForeground ( Color.WHITE );
 		}
-		SimpleScreenIO.show ( P, "Legend", 50, 200 );
+
+		widget.add ( P ); widget.setBackground ( Color.WHITE );
+		widget.setPreferredSize ( new Dimension ( 60, 400 ) );
+
+		SimpleScreenIO.show ( widget, "Legend", 60, 500 );
 	}
 	static final Widget [] EMPTY = new Widget[]{};
 
