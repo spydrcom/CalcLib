3a7361bde995eb144af47c440f6345edfec8b605
Michael Druckman <spydrcom@gmail.com>
2023-05-25 15:44:17 -0700

    Update TaylorSeriesEvaluations.java
    functional completion and code polish
    Formatter added to improve output readability
    Javadocs comments added

--- src/net/myorb/math/computational/TaylorSeriesEvaluations.java
+++ src/net/myorb/math/computational/TaylorSeriesEvaluations.java
@@ -2,10 +2,13 @@
 package net.myorb.math.computational;
 
 import net.myorb.math.computational.sampling.Calculus;
-import net.myorb.math.GeneratingFunctions.Coefficients;
+import net.myorb.math.computational.sampling.SegmentAnalysis.SampleFormatter;
 
 import net.myorb.math.expressions.ExpressionSpaceManager;
+import net.myorb.math.GeneratingFunctions.Coefficients;
 import net.myorb.math.Function;
+
+import java.text.NumberFormat;
 
 /**
  * Taylor polynomial evaluations for functions
@@ -22,20 +25,36 @@ public class TaylorSeriesEvaluations <T> extends Calculus <T>
 	 */
 
 
+	/**
+	 * build a Taylor series for a function
+	 * @param f the function for series being evaluated
+	 * @param a the point a where the Taylor series focus is
+	 * @param order the ultimate order of the polynomial being built
+	 * @param dx the run value to use for the derivative approximations
+	 * @param proximity the linear distance on each side of point a
+	 * @return the polynomial Coefficients computed for the series
+	 */
 	public Coefficients <T> compute
 		(
 			Function <T> f, T a, int order,
 			T dx, T proximity
 		)
 	{
+		this.derivatives = new Derivatives ();
 		Coefficients <T> C = new Coefficients <> ();
-		Derivatives derivatives = new Derivatives ();
 		evaluate (f, a, order, dx, proximity, derivatives);
 		compute (a, derivatives, C);
 		return C;
 	}
+	public Derivatives derivatives;
 
 
+	/**
+	 * compute Taylor series Coefficients
+	 * @param a the point a where the Taylor series focus is
+	 * @param derivatives the list collecting derivative samples
+	 * @param C the list collecting computed Coefficients
+	 */
 	public void compute
 		(
 			T a, Derivatives derivatives,
@@ -59,8 +78,34 @@ public class TaylorSeriesEvaluations <T> extends Calculus <T>
 
 	public TaylorSeriesEvaluations
 	(ExpressionSpaceManager <T> manager)
-	{ super (manager); }
+	{ super ( new Formatter <T> (manager), manager ); }
 
 
+}
+
+/**
+ * format numbers as decimal restricting digit count
+ * @param <T> data type being used
+ */
+class Formatter <T> implements SampleFormatter <T>
+{
+
+	Formatter (ExpressionSpaceManager <T> manager)
+	{
+		this.F = NumberFormat.getNumberInstance ();
+		this.F.setMaximumFractionDigits (3);
+		this.manager = manager;
+	}
+	ExpressionSpaceManager <T> manager;
+	NumberFormat F;
+
+	/* (non-Javadoc)
+	 * @see net.myorb.math.computational.sampling.SegmentAnalysis.SampleFormatter#format(java.lang.Object)
+	 */
+	public String format (T sample)
+	{
+		return F.format (manager.convertToDouble (sample)) + "\t";
+	}
+	
 }
 
